ffaaa2ce6bbb7e620acf3dd363f4db5b1cae8d91
micpas <m.pasternakiewicz@interia.pl>
2019-03-13 15:24:12 +0100

    (2) Sprawdzenie poprawności formatowania

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -13,17 +13,17 @@ public class defaultCountingOutRhymer {
 	public boolean callCheck() {
 		return total == -1;
 	}
-	
+
 	public boolean isFull() {
 		return total == 11;
 	}
-	
+
 	protected int peekaboo() {
 		if (callCheck())
 			return -1;
 		return NUMBERS[total];
 	}
-		
+
 	public int countOut() {
 		if (callCheck())
 			return -1;
